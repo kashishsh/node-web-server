85853e5bd1bd9e18066913b3a9dbc27e75865f44
Ashish kumar <shrivastav.ashish016@gmail.com>
2017-09-29 13:45:07 +0530

    Advance templating using hbs

--- server.js
+++ server.js
@@ -1,21 +1,40 @@
 const express = require('express');
+const hbs = require('hbs');
 
-const app = express();
+var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
-    res.send({
-        name: 'Ashish',
-        city: 'Agra'
-    });
-});  
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
+  });
+});
 
 app.get('/about', (req, res) => {
-    res.send('About page goes here...!!');
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
 });
 
+// /bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
-    res.send('This is a bad request...!!');
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
 });
-app.listen(3000);
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
