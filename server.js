34c7f553bad51ac340c28ddefd7b993aaca078f7
Ashish kumar <shrivastav.ashish016@gmail.com>
2017-09-29 12:51:39 +0530

    Creating web server

--- server.js
+++ server.js
@@ -2,7 +2,20 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
-    res.send('Hello Express!!');
+    res.send({
+        name: 'Ashish',
+        city: 'Agra'
+    });
 });  
+
+app.get('/about', (req, res) => {
+    res.send('About page goes here...!!');
+});
+
+app.get('/bad', (req, res) => {
+    res.send('This is a bad request...!!');
+});
 app.listen(3000);
